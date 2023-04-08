d7e6c57d8d341fb660d60f65e6eccd4ce6c8f082
Oleg <osh1977@yandex.ru>
2023-04-08 14:18:33 +0300

    Создали программу приветствия и внесли изменения вывода

---
+++ HelloUsername/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+//Команда Console.Write 
+Console.Write("Введите Ваше имя ");
+string Username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(Username);
